06a94da11c783e05dcba75d3092d5f075adfa4cd
Elizabeth Brandt <ebrandt8@uw.edu>
2021-01-08 21:24:37 -0800

    added starting files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Double Bacon Cheeseburger", false),
+    ("Bacon Bleu Cheeseburger", false),
+    ("Peanut Butter Pickle Cheeseburger", false),
+    ("BBQ Burger", true)
