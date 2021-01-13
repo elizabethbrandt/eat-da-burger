b2b2ac7e3cee8528b4d0326c7086d2e786626609
Elizabeth Brandt <ebrandt8@uw.edu>
2021-01-12 20:05:05 -0800

    completed put route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -24,6 +24,22 @@ router.post("/api/burgers", (req, res) => {
 
         res.json({id: data.id})
     })
+});
+
+router.put("/api/burgers/:id", (req, res) => {
+
+    const condition = `id = ${req.params.id}`
+
+    burgers.update({
+        devoured: req.body.devoured}, condition, (data) => {
+
+        if(data.changedRows === 0) {
+            return res.status(404).end();
+        } else{
+            res.status(200).end();
+        }
+
+    })
 })
 
 module.exports = router;
