9207c42dbe0bba948f6fa2cfa33ac997b8f5fb4d
Elizabeth Brandt <ebrandt8@uw.edu>
2021-01-12 21:29:47 -0800

    trying to fix update method

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,32 @@
 const connection = require("./connection");
 
+const printQuestionMarks = num => {
+    const arr = [];
+  
+    for (let i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+}
+
+const objToSql = ob => {
+    const arr = [];
+  
+    for (let key in ob) {
+      let value = ob[key];
+
+      if (Object.hasOwnProperty.call(ob, key)) {
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        arr.push(key + "=" + value);
+      }
+    }
+  
+    return arr.toString();
+}
+
 const orm = {
 
     all: (table, cbModel) => {
@@ -18,7 +45,7 @@ const orm = {
 
     create: (table, cols, vals, cbModel) => {
 
-        let queryString = `INSERT INTO ${table} (${cols.toString()}) VALUES (?,?,?)`
+        let queryString = `INSERT INTO ${table} (${cols.toString()}) VALUES (${printQuestionMarks(vals)})`
 
         console.log(queryString + "CREATE QS");
 
@@ -26,13 +53,13 @@ const orm = {
 
             if (err) throw err;
 
-            cbModel(res)
+            cbModel(res);
         })
     },
 
     update: (table, vals, condition, cbModel) => {
 
-        let queryString = `UPDATE ${table} SET ${vals} WHERE ${condition};`
+        let queryString = `UPDATE ${table} SET ${objToSql(vals)} WHERE ${condition};`
 
         console.log(queryString + "UPDATE QS");
 
