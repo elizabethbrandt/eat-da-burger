59e5c3e7316e25ee8629ef4409f6a38c216bf4a2
Elizabeth Brandt <ebrandt8@uw.edu>
2021-01-12 20:05:29 -0800

    fixed update method

--- config/orm.js
+++ config/orm.js
@@ -1,30 +1,46 @@
-const connection = require("../config/connection");
+const connection = require("./connection");
 
 const orm = {
 
-    all: (tableInput, cbModel) => {
+    all: (table, cbModel) => {
 
-        let queryString = "SELECT * FROM " + tableInput + ";";
+        let queryString = `SELECT * FROM ${table};`;
+
+        console.log(queryString + "ALL QS");
 
         connection.query(queryString, (err, res) => {
 
             if (err) throw err;
-            
+
             cbModel(res);
         });
     },
 
     create: (table, cols, vals, cbModel) => {
 
-        let queryString = "INSERT INTO " + table + "(" + cols.toString() + ") VALUES (?,?,?)"
+        let queryString = `INSERT INTO ${table} (${cols.toString()}) VALUES (?,?,?)`
 
-        console.log(queryString);
+        console.log(queryString + "CREATE QS");
 
         connection.query(queryString, vals, (err, res) => {
 
             if (err) throw err;
 
             cbModel(res)
+        })
+    },
+
+    update: (table, vals, condition, cbModel) => {
+
+        let queryString = `UPDATE ${table} SET ${vals} WHERE ${condition};`
+
+        console.log(queryString + "UPDATE QS");
+
+        connection.query(queryString, (err, res) => {
+            
+            if (err) throw err;
+
+            cbModel(res);
         })
     }
 };
